5abb37a7e7725c86e4e1b995039584c3117b7bf1
Ethan Chew <ethanchewmh@gmail.com>
2021-10-19 20:54:26 +0800

    Meta Tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 import { ChakraProvider, extendTheme, VStack } from "@chakra-ui/react"
 import Footer from '../components/Footer';
 import NavBar from '../components/Navbar';
@@ -14,6 +15,22 @@ const theme = extendTheme({
 function App({ Component, pageProps }) {
   return(
       <ChakraProvider theme={theme}>
+        <Head>
+          <meta name="title" content="Ethan Chew" />
+          <meta name="description" content="I'm Ethan Chew, a 15 year old, studying Computing in the School of Science and Technology, Singapore." />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://www.ethanchew.com/" />
+          <meta property="og:title" content="Ethan Chew" />
+          <meta property="og:description" content="I'm Ethan Chew, a 15 year old, studying Computing in the School of Science and Technology, Singapore." />
+          <meta property="og:image" content="https://www.ethanchew.com/assets/img/Social%20Media%20Img.png" />
+
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://www.ethanchew.com/" />
+          <meta property="twitter:title" content="Ethan Chew" />
+          <meta property="twitter:description" content="I'm Ethan Chew, a 15 year old, studying Computing in the School of Science and Technology, Singapore." />
+          <meta property="twitter:image" content="https://www.ethanchew.com/assets/img/Social%20Media%20Img.png" />
+        </Head>
         <NavBar />
         <VStack spacing={50}>
             <Component {...pageProps} />
