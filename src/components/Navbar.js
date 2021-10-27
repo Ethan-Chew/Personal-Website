f31624becfb8cc7c421c466fe0a2999af5294a7f
Ethan Chew <ethanchewmh@gmail.com>
2021-10-28 07:46:39 +0800

    New NavBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,13 @@ import {
     Box,
     Flex,
     HStack,
+    Stack,
     Image,
     Button,
     useColorModeValue,
     useColorMode,
+    useDisclosure,
+    IconButton
 } from '@chakra-ui/react';
 
 import {
@@ -15,36 +18,78 @@ import {
     SunIcon,
 } from '@chakra-ui/icons';
 
-import { ReactNode } from 'react';
+import Link from 'next/link';
 
 const Pages = [{
   id: "Home",
   href: "/"
 }, {
-  id: "Projects",
-  href: "/Projects"
+  id: "Profile",
+  href: "https://profile.ethanchew.com"
+}, {
+  id: "Blog",
+  href: "https://blog.ethanchew.com"
+}, {
+  id: "Web Apps",
+  href: "https://web-apps.ethanchew.com"
 }]
+
+const NavLink = ({ children }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={'md'}
+    _hover={{
+      textDecoration: 'none',
+      bg: useColorModeValue('gray.200', 'gray.700'),
+    }}
+    href={children.href} 
+    passHref={(children.id === "Home") ? false : true}>
+    {children.id}
+  </Link>
+);
 
 export default function NavBar() {
     const { colorMode, toggleColorMode } = useColorMode();
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
     return(
-      <>
-        <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4} boxShadow="md">
-          <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-            
-            <HStack spacing={8} alignItems={'center'}>
-                {colorMode === 'light' ? <Image src="/Name Logo.png" alt="Ethan Chew" width={200} borderRadius="md" /> : <Image src="/Name Logo L.png" alt="Ethan Chew" width={200} borderRadius="md" />}
+      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <IconButton
+            size={'md'}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={'Open Menu'}
+            display={{ md: 'none' }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={'center'}>
+            {colorMode === 'light' ? <Image src="/Name Logo.png" alt="Ethan Chew" width={200} borderRadius="md" /> : <Image src="/Name Logo L.png" alt="Ethan Chew" width={200} borderRadius="md" />}
+            <HStack
+              as={'nav'}
+              spacing={6}
+              display={{ base: 'none', md: 'flex' }}>
+              {Pages.map((page) => (
+                <NavLink key={page.id}>{page}</NavLink>
+              ))}
             </HStack>
-
-            <Flex alignItems={'center'}>
-                <Button onClick={toggleColorMode}>
-                    {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
-                </Button>
-            </Flex>
-    
+          </HStack>
+          <Flex alignItems={'center'}>
+            <Button onClick={toggleColorMode}>
+              {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+            </Button>
           </Flex>
-        </Box>
-      </>
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: 'none' }}>
+            <Stack as={'nav'} spacing={4}>
+              {Pages.map((page) => (
+                <NavLink key={page.id}>{page}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+    </Box>
     )
 }
