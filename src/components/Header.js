09dc0752df3f107bfe267e6c07f50d286cc13a91
Ethan Chew <chew_ming_hong_ethan@s2019.ssts.edu.sg>
2023-02-09 00:41:50 +0800

    Use escape characters

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,9 +25,9 @@ export default function Header() {
         </VStack>
         <Divider />
         <VStack spacing={5} alignItems="left">
-          <Text>Hi! I'm Ethan Chew, a <b>{new Date().getFullYear() - 2006}-year-old student</b> from Singapore who will be <b>studying Information Technology at Ngee Ann Polytechnic</b>. In addition, I am an <b>Intern at Tinkertanker</b>, developing Swift and SwiftUI-related curriculum.</Text>
+          <Text>Hi! I&apos;m Ethan Chew, a <b>{new Date().getFullYear() - 2006}-year-old student</b> from Singapore who will be <b>studying Information Technology at Ngee Ann Polytechnic</b>. In addition, I am an <b>Intern at Tinkertanker</b>, developing Swift and SwiftUI-related curriculum.</Text>
           <Text>I have a bunch of interests like aviation, science and photography to name a few. I only really started programming in 2020 <i>(Thats... {new Date().getFullYear() - 2020} years ago)</i> when I entered secondary school, so I am constantly learning, and have a looong way to go!</Text>
-          <Text>I'm always happy to meet new people, you can contact me by pressing any of the buttons below!</Text>
+          <Text>I&apos;m always happy to meet new people, you can contact me by pressing any of the buttons below!</Text>
         </VStack>
         <HStack alignItems="left">
           <IconButton icon={<FaGithub size={22} />} aria-label={"GitHub"}  onClick={() => router.push(links.Github)}/>
